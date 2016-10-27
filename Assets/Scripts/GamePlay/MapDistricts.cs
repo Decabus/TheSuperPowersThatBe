ebbdc796da730b9e16ff54eea19bdde15b07c6ce
Declan Peach <Declan.K.Peach@gmail.com>
2016-10-27 11:56:36 +0100

    +Added District GUI

--- Assets/Scripts/GamePlay/MapDistricts.cs
+++ Assets/Scripts/GamePlay/MapDistricts.cs
@@ -10,14 +10,23 @@ public class MapDistricts : MonoBehaviour{
 	[SerializeField]
 	GameObject districtMesh;
 
+	Round2Manager r2M;
+
+	void Start(){
+		r2M = GameObject.Find ("Stage2Canvas").GetComponent<Round2Manager> ();
+	}
+
 	void OnMouseOver(){
-		panel.SetActive (true);
+		//panel.SetActive (true);
 		//panel.GetComponent<Image> ().enabled = true;
 		districtMesh.GetComponent<Renderer> ().material.color = new Color (1.0f, 0.1f, 0.1f);
+		if (Input.GetMouseButtonUp (0)) {
+			r2M.activatePanel (panel);
+		}
 	}
 
 	void OnMouseExit(){
-		panel.SetActive (false);
+		//panel.SetActive (false);
 		districtMesh.GetComponent<Renderer> ().material.color = new Color (1.0f, 1.0f, 1.0f);
 	}
 }
---
+++ Assets/Scripts/GamePlay/Round2Manager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Round2Manager : MonoBehaviour {
+
+	//public GameObject activePanel;
+
+	[SerializeField]
+	private List<GameObject> panels;
+
+	public void activatePanel(GameObject panelToActiv){
+		foreach(GameObject i in panels){
+			if (panelToActiv.name == i.name) {
+				i.SetActive (true);
+			} else {
+				i.SetActive (false);
+			}
+		}
+	}
+}
