d95fcd8dde63c49db466d67845bb7e9474cedcda
Zer0Naught <lukenorman91@gmail.com>
2016-10-28 04:37:14 +0100

    SplashGUI

---
+++ Assets/Scripts/FadeToBlack.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class FadeToBlack : MonoBehaviour {
+    Image fader;
+	// Use this for initialization
+	void Start () {
+        FadeOut();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+    public IEnumerator FadeIn()
+    {
+        StopCoroutine("FadeOut");
+        fader.color = Color.black;
+        while (fader.color.a > 0)
+        {
+            fader.color = Color.Lerp(fader.color, Color.clear, 1 * Time.deltaTime);
+            if (fader.color.a < 0.02f)
+                fader.color = Color.clear;
+            yield return null;
+        }
+    }
+
+    public IEnumerator FadeOut()
+    {
+        StopCoroutine("FadeIn");
+        fader.color = Color.clear;
+        while (fader.color.a < 1)
+        {
+            fader.color = Color.Lerp(fader.color, Color.black, 1 * Time.deltaTime);
+            if (fader.color.a > 0.98f)
+                fader.color = Color.black;
+            yield return null;
+        }
+    }
+}
